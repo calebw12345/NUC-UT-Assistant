79f94a5fe2e07ef41362f4dd3ef54a2b9b276432
Caleb Watson <95868575+calebw12345@users.noreply.github.com>
2026-04-14 16:57:22 -0400

    Update NUC-UT_Assistant.py

--- NUC-UT_Assistant.py
+++ NUC-UT_Assistant.py
@@ -22,11 +22,7 @@ from datetime import datetime
 # ==============================
 # CONFIG
 # ==============================
-with st.sidebar:
-    OPENAI_API_KEY = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 client = OpenAI(api_key=OPENAI_API_KEY)
 
 MODEL_NAME = "gpt-4o-mini"
